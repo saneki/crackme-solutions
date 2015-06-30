1d9ec20dbc6e4ce5f45a01bd55e643f8aea22b41
saneki <s@neki.me>
2015-06-30 18:24:13 -0500

    [t0adk3yg3n] Fixed starting xor byte calculation

--- solutions/T0ADK3YG3N-jockcranley/keygen.c
+++ solutions/T0ADK3YG3N-jockcranley/keygen.c
@@ -9,7 +9,23 @@
 
 #include <stdio.h>
 
-// Area of interest @004014ED (win32 program)
+// Areas of interest: @004014C5, @004014ED (win32 program)
+// -> @004014C5 generates the starting xor byte from username[1]
+// -------------------------------------------------------------
+// 004014C5 | 0F B6 44 24 24   | movzx eax,byte ptr ss:[esp+24]| ; eax: usernameStr[1]
+// 004014CA | 66 0F BE D0      | movsx dx,al                   |
+// 004014CE | 6B D2 56         | imul edx,edx,56               |
+// 004014D1 | 66 C1 EA 08      | shr dx,8                      |
+// 004014D5 | C0 F8 07         | sar al,7                      |
+// 004014D8 | 89 D3            | mov ebx,edx                   |
+// 004014DA | 29 C3            | sub ebx,eax                   |
+// 004014DC | 89 D8            | mov eax,ebx                   |
+// 004014DE | 0F BE C0         | movsx eax,al                  |
+// 004014E1 | 89 44 24 3C      | mov dword ptr ss:[esp+3C],eax |
+// -------------------------------------------------------------
+// ...
+// -> @004014ED generates the good password
+// -------------------------------------------------------------
 // 004014ED | EB 41            | jmp toadkey32.401530          |
 // 004014EF | 8D 54 24 23      | lea edx,dword ptr ss:[esp+23] | ; edx: Username string
 // 004014F3 | 8B 44 24 34      | mov eax,dword ptr ss:[esp+34] | ; Index
@@ -33,11 +49,14 @@
 // 0040152B | 83 44 24 34 01   | add dword ptr ss:[esp+34],1   |
 // 00401530 | 83 7C 24 34 08   | cmp dword ptr ss:[esp+34],8   |
 // 00401535 | 7E B8            | jle toadkey32.4014EF          |
+// -------------------------------------------------------------
 void keygen(const char *username, char *key)
 {
 	int temp;
 	char c;
-	char xorc = '#';
+
+	// Starting xor byte
+	char xorc = ((unsigned short)(username[1] * 0x56)) >> 8;
 
 	for(int i = 0; i < 8; i++)
 	{
